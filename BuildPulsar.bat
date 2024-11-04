f9dc1f339446351917690ffb56c6affb2b4e4210
patchzyy <64382339+patchzyy@users.noreply.github.com>
2024-11-04 11:17:52 +0100

    Improved bat

--- BuildPulsar.bat
+++ BuildPulsar.bat
@@ -1,58 +1,94 @@
-
-SETLOCAL EnableDelayedExpansion
 @echo off
+SETLOCAL EnableDelayedExpansion
 cls
-del build\*.o
 
-::DEBUG only works if you have the map and readelf (which is part of MinGW)
+:: Clear old build files
+echo Cleaning up old build files...
+if exist build\*.o (
+    del /q build\*.o
+    if %ErrorLevel% neq 0 (
+        echo Error: Failed to delete old object files in build directory.
+        exit /b 1
+    )
+) else (
+    echo No old object files to delete.
+)
+
+:: Check for debug flag and configure
 SET "debug="
-::SET "cwDWARF="
-::if "%1" equ "-d" SET "debug=-debug -map=^"Dolphin Emulator\Maps\RMCP01.map^" -readelf=^"C:\MinGW\bin\readelf.exe^""
-::if "%1" equ "-d" SET "cwDWARF=-g"
+:: Uncomment the next two lines to enable debug settings based on arguments
+:: SET "cwDWARF="
+:: if "%1" equ "-d" SET "debug=-debug -map=^"Dolphin Emulator\Maps\RMCP01.map^" -readelf=^"C:\MinGW\bin\readelf.exe^"" && SET "cwDWARF=-g"
 
-:: Sources and Compiler
-SET "ENGINE=.\KamekInclude"
-set "GAMESOURCE=.\GameSource"
-SET "PULSAR=.\PulsarEngine"
+:: Define paths
+SET "ENGINE=G:\Coding\MarioKart\RR\Common\KamekInclude"
+SET "GAMESOURCE=G:\Coding\MarioKart\RR\Common\GameSource"
+SET "PULSAR=G:\Coding\MarioKart\RR\Common\PulsarEngine"
+SET "CC=C:\CodeWarriors\Command_Line_Tools\mwcceppc.exe"
+SET "RIIVO=C:\Users\patchzy\AppData\Roaming\Dolphin Emulator\Load\Riivolution\Pulsar2\RetroRewind6\Binaries\new"
+SET "CFLAGS=-I- -i %ENGINE% -i %GAMESOURCE% -i %PULSAR% -opt all -inline auto -enum int -proc gekko -fp hard -sdata 0 -sdata2 0 -maxerrors 1 -func_align 4 %cwDWARF%"
 
-:: Change this as necessary depending on where you put CodeWarrior
-SET "CC="
-
-:: Riivolution Destination (change as necessary)
-SET "RIIVO="
-
-:: Compiler flags and folder
-SET CFLAGS=-I- -i %ENGINE% -i %GAMESOURCE% -i %PULSAR% ^
-  -opt all -inline auto -enum int -proc gekko -fp hard -sdata 0 -sdata2 0 -maxerrors 1 -func_align 4 %cwDWARF%
-SET DEFINE=
-
-if "!CC!" == "" (
-    echo You need to specify the path to CodeWarrior by setting the CC variable
+:: Ensure the CodeWarrior path exists
+if not exist "%CC%" (
+    echo Error: CodeWarrior path not found. Please set the correct path for CC.
     exit /b 1
 )
 
-:: CPP Sources
+:: Ensure the necessary directories exist
+if not exist "build" (
+    mkdir build
+    if %ErrorLevel% neq 0 (
+        echo Error: Failed to create build directory.
+        exit /b 1
+    )
+)
+
+:: Collect .cpp files
 SET CPPFILES=
-for /R %PULSAR% %%f in (*.cpp) do SET "CPPFILES=%%f !CPPFILES!"
+for /R %PULSAR% %%f in (*.cpp) do (
+    SET "CPPFILES=!CPPFILES! %%f"
+)
 
-:: Compile CPP
+:: Compile kamek.cpp
+echo Compiling kamek.cpp...
 %CC% %CFLAGS% -c -o "build/kamek.o" "%ENGINE%\kamek.cpp"
+if %ErrorLevel% neq 0 (
+    echo Error: Failed to compile kamek.cpp.
+    exit /b 1
+)
 
+:: Compile each .cpp file and add to OBJECTS list
 SET OBJECTS=
 FOR %%H IN (%CPPFILES%) DO (
-    ::echo "Compiling %%H..."
+    echo Compiling %%H...
     %CC% %CFLAGS% %DEFINE% -c -o "build/%%~nH.o" "%%H"
-    SET "OBJECTS=build/%%~nH.o !OBJECTS!"
+    if %ErrorLevel% neq 0 (
+        echo Error: Failed to compile %%H.
+        exit /b 1
+    )
+    SET "OBJECTS=!OBJECTS! build/%%~nH.o"
 )
 
-:: Link
+:: Linking
 echo Linking... %time%
 ".\KamekLinker\Kamek.exe" "build/kamek.o" %OBJECTS% %debug% -dynamic -externals="%GAMESOURCE%/symbols.txt" -versions="%GAMESOURCE%/versions.txt" -output-combined=build\Code.pul
-
-if %ErrorLevel% equ 0 if NOT "!RIIVO!" == "" (
-    xcopy /Y build\*.pul "%RIIVO%\Binaries" /i /q
-    echo Binaries copied
+if %ErrorLevel% neq 0 (
+    echo Error: Linking failed.
+    exit /b 1
 )
 
+:: Copy output to Riivolution folder
+if defined RIIVO (
+    echo Copying binaries to %RIIVO%\Binaries...
+    xcopy /Y build\*.pul "%RIIVO%\Binaries" /i /q
+    if %ErrorLevel% neq 0 (
+        echo Error: Failed to copy binaries to %RIIVO%\Binaries.
+        exit /b 1
+    ) else (
+        echo Binaries copied successfully.
+    )
+)
+
+echo Build completed successfully.
 :end
 ENDLOCAL
