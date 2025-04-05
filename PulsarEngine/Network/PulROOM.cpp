b5ffa8e6e518ca3bfca3da567c018a25617cde4d
Rambo6Glaz <39063367+EpicUsername12@users.noreply.github.com>
2025-04-06 01:39:59 +0900

    Improve Extended Teams sync

--- PulsarEngine/Network/PulROOM.cpp
+++ PulsarEngine/Network/PulROOM.cpp
@@ -212,7 +212,8 @@ static void AfterROOMReception(const RKNet::PacketHolder<PulROOM>* packetHolder,
         }
     }
 
-    if (src.messageType == UI::ExtendedTeamManager::MSG_TYPE_UPDATE_TEAMS && !isHost && packetHolder->packetSize == sizeof(PulROOM)) {
+    const bool isExtendedTeamsUpdatableMessage = (src.messageType == UI::ExtendedTeamManager::MSG_TYPE_UPDATE_TEAMS) || (src.messageType == UI::ExtendedTeamManager::MSG_TYPE_START_RACE);
+    if (isExtendedTeamsUpdatableMessage && !isHost && packetHolder->packetSize == sizeof(PulROOM)) {
         HandleExtendedTeamUpdates(src);
     }
 
