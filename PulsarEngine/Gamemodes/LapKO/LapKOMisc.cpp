b1f8c66d5f681b89c2d886e18764a593e8a53237
ZPL <158531259+ZacharyPL@users.noreply.github.com>
2025-10-12 11:11:57 -0500

    Expand and refactor WiFi time limit and disconnect logic

--- PulsarEngine/Gamemodes/LapKO/LapKOMisc.cpp
+++ PulsarEngine/Gamemodes/LapKO/LapKOMisc.cpp
@@ -16,10 +16,22 @@ static RaceFrameHook lapKoFrameHook(FrameUpdate);
 
 kmRuntimeUse(0x8053F3B8); // Wifi Time Limit Expansion [Chadderz]
 kmRuntimeUse(0x8053F3BC);
-static void WifiTimeLimit() {
+kmRuntimeUse(0x80521408); // No Disconnect [Bully]
+kmRuntimeUse(0x8053EC94);
+kmRuntimeUse(0x8053EF6C);
+kmRuntimeUse(0x8053F0B4);
+kmRuntimeUse(0x8053F124);
+static void WifiEdits() {
     // Default is 5 minutes (300k Milliseconds)
     kmRuntimeWrite32A(0x8053F3B8, 0x3C600005);
     kmRuntimeWrite32A(0x8053F3BC, 0x388393E0);
+
+    // Default disconnect behavior
+    kmRuntimeWrite32A(0x80521408, 0x38030001);
+    kmRuntimeWrite32A(0x8053EC94, 0x38040001);
+    kmRuntimeWrite32A(0x8053EF6C, 0x38030001);
+    kmRuntimeWrite32A(0x8053F0B4, 0x38030001);
+    kmRuntimeWrite32A(0x8053F124, 0x38030001);
 
     System* system = System::sInstance;
     if (system == nullptr) return;
@@ -30,8 +42,15 @@ static void WifiTimeLimit() {
     // 15 minutes if LapKO is enabled (900k Milliseconds)
     kmRuntimeWrite32A(0x8053F3B8, 0x3C60000D);
     kmRuntimeWrite32A(0x8053F3BC, 0x6064BBA0);
+
+    // Disable disconnects from being idle
+    kmRuntimeWrite32A(0x80521408, 0x38000000);
+    kmRuntimeWrite32A(0x8053EC94, 0x38000000);
+    kmRuntimeWrite32A(0x8053EF6C, 0x38000000);
+    kmRuntimeWrite32A(0x8053F0B4, 0x38000000);
+    kmRuntimeWrite32A(0x8053F124, 0x38000000);
 }
-static SectionLoadHook wifiTimeLimitHook(WifiTimeLimit);
+static SectionLoadHook WifiEditsHook(WifiEdits);
 
 }  // namespace LapKO
 }  // namespace Pulsar
